9e65d1ac94cb77a6874dbb1e0db9951bc2fcef7b
BinZhu <30952106+binzh93@users.noreply.github.com>
2018-07-03 13:04:43 +0800

    Update train.py

--- train.py
+++ train.py
@@ -1,8 +1,8 @@
 import sys
 sys.path.append('/workspace/mnt/group/face-det/zhubin/caffe/python')
 import caffe
-caffe.set_device(0)
+caffe.set_device(1)
 caffe.set_mode_gpu()
 solver = caffe.SGDSolver('/workspace/mnt/group/face-det/zhubin/face_resnet18/solver.prototxt')
-solver.net.copy_from('/workspace/mnt/group/face-det/zhubin/train_file/resnet18-priv.caffemodel')
+solver.net.copy_from('/workspace/mnt/group/face-det/zhubin/train_file/resnet-18.caffemodel')
 solver.solve()
