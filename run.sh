c478646fd1f92980b030f1b54946664d0153cf21
BinZhu <30952106+binzh93@users.noreply.github.com>
2018-07-06 09:46:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
-nohup python train.py > GenderAge.log 2>&1 &
+cd /workspace/mnt/group/face-det/zhubin/face_resnet18/
+echo '===>Start training!' #>> GenderAge.log
+python train.py
+echo '===>Training finished!' #>> GenderAge.log
